0c8f9554e4b7fbf5a296d73f2e8145cd1f371606
Hiwot <54193773+Hiwot985@users.noreply.github.com>
2022-06-27 23:40:22 -0400

    Adding a file to submit assignment

---
+++ Peer graded project.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h4>This is a test for the peer graded project</h4>
+
+# In[1]:
+
+
+def gauss(n):
+    return (n*(n+1))/2 
+gauss(100) == sum(range(101))
+
+
+# In[ ]:
+
+
+
+
